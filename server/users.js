258b65dd162509edacf32a335da6d431507995d4
dchop <devanshchopra@hotmail.com>
2020-09-14 21:46:44 -0700

    helper user functions implemented

--- server/users.js
+++ server/users.js
@@ -16,13 +16,14 @@ const addUser = ({id, name, room}) => {
 }
 
 const removeUser = () => {
-
+    const index = users.findIndex((user) => user.id === id);
+    if (index !== -1){
+        return users.splice(index, 1)[0];
+    }
 }
 
-const getUser = () => {
+const getUser = (id) => users.find((user) => user.id === id);
 
-}
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-const getUsersInRoom = () => {
-
-}
+module.exports = {addUser, removeUser, getUser, getUsersInRoom};
