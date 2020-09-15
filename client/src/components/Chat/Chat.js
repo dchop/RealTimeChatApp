688a7c9ec3072ab25bcc1bdef4617ce979a5607c
dchop <devanshchopra@hotmail.com>
2020-09-14 21:24:42 -0700

    AddUser function for backend created

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,8 +17,14 @@ const Chat = ({ location }) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name, room}, ({error}) =>
-        alert(error));
+        socket.emit('join', {name, room}, () => {
+
+        });
+
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
     }, [ENDPOINT, location.search]);
     return (
         <h1>Chat</h1>
